8a1d2427d19d2d0dade04594c44599d3fbb7db9e
Yuvi Dodia <yuvi.dodia@gmail.com>
2025-03-04 09:13:34 -0500

    Moved template code to HTML

--- apps/draft-viewer/src/app/player-list/player-list.component.ts
+++ apps/draft-viewer/src/app/player-list/player-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 interface Player {
   grade: string;
@@ -12,74 +13,9 @@ interface Player {
 @Component({
   selector: 'app-player-list',
   standalone: true,
-  template: `
-    <div class="cards-grid">
-      <!-- Recent Players -->
-      <div class="list-card">
-        <div class="header">
-          <h2>Your Recent Players</h2>
-          <button class="see-all">See all</button>
-        </div>
-        <div class="player-list">
-          <div class="list-header">
-            <span>Grade</span>
-            <span>Age</span>
-            <span>Name</span>
-            <span>Tryout</span>
-            <span>Last Year</span>
-          </div>
-          <div class="player-row" *ngFor="let player of recentPlayers">
-            <span>{{player.grade}}</span>
-            <span>{{player.age}}</span>
-            <span class="name">{{player.name}}</span>
-            <span class="rating">{{player.tryoutRating}}</span>
-            <span class="rating">{{player.lastYearRating}}</span>
-          </div>
-        </div>
-      </div>
-
-      <!-- Top 3rd Graders -->
-      <div class="list-card">
-        <div class="header">
-          <h2>Top 3rd Grade Players</h2>
-          <button class="see-all">See all</button>
-        </div>
-        <div class="player-list">
-          <div class="list-header">
-            <span>Age</span>
-            <span>Name</span>
-            <span>Rating</span>
-          </div>
-          <div class="player-row" *ngFor="let player of thirdGradePlayers">
-            <span>{{player.age}}</span>
-            <span class="name">{{player.name}}</span>
-            <span class="rating">{{player.currentRating}}</span>
-          </div>
-        </div>
-      </div>
-
-      <!-- Top 2nd Graders -->
-      <div class="list-card">
-        <div class="header">
-          <h2>Top 2nd Grade Players</h2>
-          <button class="see-all">See all</button>
-        </div>
-        <div class="player-list">
-          <div class="list-header">
-            <span>Age</span>
-            <span>Name</span>
-            <span>Rating</span>
-          </div>
-          <div class="player-row" *ngFor="let player of secondGradePlayers">
-            <span>{{player.age}}</span>
-            <span class="name">{{player.name}}</span>
-            <span class="rating">{{player.currentRating}}</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  `,
-  styleUrls: ['./player-list.component.scss']
+  templateUrl: './player-list.component.html',
+  styleUrls: ['./player-list.component.scss'],
+  imports: [CommonModule]
 })
 export class PlayerListComponent {
   recentPlayers: Player[] = [
