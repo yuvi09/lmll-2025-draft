84b5c8a39bee9a97f91475c30b28f667e4739a4e
Yuvi Dodia <yuvi.dodia@gmail.com>
2025-03-05 11:36:00 -0500

    left nav collapsible

--- apps/draft-viewer/src/app/app.component.ts
+++ apps/draft-viewer/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   currentPage = 'Top Picks';
+  isNavCollapsed = false;
+
+  toggleNav() {
+    this.isNavCollapsed = !this.isNavCollapsed;
+  }
 }
